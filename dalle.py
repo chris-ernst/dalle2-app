b072013c790dcc1fbc766646e6ffd9b5b1a0a329
Christian Ernst <chris@MacBook-Pro-von-Christian.local>
2022-08-04 09:12:19 +0200

    DB changes

--- dalle.py
+++
@@ -1,9 +0,0 @@
-from dalle2 import Dalle2
-dalle = Dalle2("sess-PpNMhxpECX3BpvXuyhaWKSJhiKUiwfp3pX1PlkEU")
-
-generations = dalle.generate("A few days ago a flood shocked the Valcamonica, snatching the fruits of a whole life of sacrifices from too many people. Citizens deserve quick and concrete solutions: the Lombardy Region responded immediately with the first € 5 million, waiting for the government to decree a State of Emergency.")
-print(generations)
-
-file_paths = dalle.download(generations)
-print(file_paths)
-
